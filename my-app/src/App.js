070606fad18818cd35f66b007b332c3a8dc841c3
Anika Bjerknes <anika@Anikas-MacBook-Pro.local>
2023-10-06 17:34:27 -0500

    chakra

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import * as React from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
   return (
+    <ChakraProvider>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +22,7 @@ function App() {
         </a>
       </header>
     </div>
+    </ChakraProvider>
   );
 }
 
