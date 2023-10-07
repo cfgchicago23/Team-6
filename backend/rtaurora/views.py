aff069351a0161d61a6877e38d41e2d340eb1010
Ethan Arroyo <ethanrarroyo@gmail.com>
2023-10-06 22:14:37 -0500

    Added HouseView

--- backend/rtaurora/views.py
+++ backend/rtaurora/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .serializers import VolunteerSerializer, OrganizationSerializer
-from .models import Volunteer, Organization
+from .serializers import VolunteerSerializer, OrganizationSerializer, HouseSerializer
+from .models import Volunteer, Organization, House
+
 
 class VolunteerView (viewsets.ModelViewSet):
     serializer_class = VolunteerSerializer
     queryset = Volunteer.objects.all()
-    
+
+
 class OrganizationView (viewsets.ModelViewSet):
     serializer_class = OrganizationSerializer
     queryset = Organization.objects.all()
+
+
+class HouseView (viewsets.ModelViewSet):
+    serializer_class = HouseSerializer
+    queryset = House.objects.all()
