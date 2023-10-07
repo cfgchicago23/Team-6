843df72a5924375b885470edffa99bc646a4e6c2
lchen25 <lchen25@cs.washington.edu>
2023-10-06 21:44:21 -0500

    Added all fields to Volunteer

--- backend/rtaurora/models.py
+++ backend/rtaurora/models.py
@@ -9,8 +9,47 @@ STATE_CHOICES = (
     ("MI", "Michigan"),
 )
 
-TYPE = (
+PREFERENCE = (
+    ("email", "Email"),
+    ("phone", "Phone"),
+)
 
+SHIRT = (
+    ("xsmall", "XSmall"),
+    ("small", "Small"),
+    ("medium", "Medium"),
+    ("large", "Large"),
+    ("xlarge", "XLarge"),
+    ("2xlarge", "2XLarge"),
+    ("3xlarge", "3XLarge"),
+)
+
+AVAILABILITY = (
+    ("asneeded", "As Needed"),
+    ("weekdays", "Weekdays"),
+    ("weekends", "Weekends"),
+)
+
+ROLE = (
+    ("captain", "House Captain"),
+    ("cocaptain", "Co-Captain"),
+    ("professional", "Professional/Skilled Volunteer"),
+    ("general", "General/Community Volunteer"),
+    ("office", "Office Volunteer"),
+    ("photographer", "Photographer"),
+)
+
+EXPERTISE = (
+    ("general", "General Volunteer"),
+    ("carpentry", "Carpentry"),
+    ("window", "Window replacement"),
+    ("drywall", "Drywall/Plaster work"),
+    ("flooring", "Flooring"),
+    ("painting", "Painting"),
+    ("hvac", "HVAC"),
+    ("plumbing", "Plumbing"),
+    ("electrical", "Electrical"),
+    ("landscaping", "Landscaping"),
 )
 
 # Create your models here.
@@ -19,6 +58,20 @@ TYPE = (
 class Volunteer(models.Model):
     name = models.CharField(max_length=MAX_LENGTH)
     email = models.EmailField(max_length=MAX_LENGTH, blank=True)
+    phone = models.IntegerField(default=0)
+    preference = models.CharField(max_length=20, choices=PREFERENCE, default="email")
+    street = models.TextField(blank=True)
+    state = models.CharField(max_length=MAX_LENGTH, choices=STATE_CHOICES, default="na")
+    city = models.TextField(blank=True)
+    zipCode = models.IntegerField(default=0000)
+    dob = models.DateField(default='2023-10-07')
+    isAdult =  models.BooleanField(default=True)
+    shirt = models.CharField(max_length=20, choices=SHIRT, default="small")
+    availability = models.CharField(max_length=20, choices=AVAILABILITY, default="asneeded")
+    role = models.CharField(max_length = 20, choices=EXPERTISE, default="general")
+    other = models.TextField(blank=True)
+    reference = models.TextField(blank=True)
+
 
     def __str__(self) -> str:
         return self.name
