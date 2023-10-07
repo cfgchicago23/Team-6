5e757edd21fed646f3cb68b0a9e4645624bf25e8
meganleongg <98578642+meganleongg@users.noreply.github.com>
2023-10-06 19:28:00 -0500

    unstyled homepage stats impact bar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,28 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import * as React from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
+import Home from './pages/home';
 
 function App() {
   return (
     <ChakraProvider>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home></Home>
     </div>
     </ChakraProvider>
   );
+
+
 }
 
 export default App;
