65165fcf027bb0167e5551adb880b479c03d172f
Pranav Chati <pchati.hack@gmail.com>
2023-10-06 19:29:22 -0500

    created organization model in models.py

--- backend/rtaurora/models.py
+++ backend/rtaurora/models.py
@@ -2,6 +2,16 @@ from django.db import models
 
 MAX_LENGTH = 255
 
+STATE_CHOICES = (
+    ("na", "Choose State"),
+    ("OH", "Ohio"),
+    ("MI", "Michigan"),
+)
+
+TYPE = (
+
+)
+
 # Create your models here.
 class Volunteer(models.Model):
     name = models.CharField(max_length=MAX_LENGTH)
@@ -9,3 +19,14 @@ class Volunteer(models.Model):
 
     def __str__(self) -> str:
         return self.name
+    
+
+class Organization (models.Model):
+    name = models.CharField(max_length=MAX_LENGTH)
+    street = models.TextField()
+    state = models.CharField(max_length=2, choices=STATE_CHOICES, default="na")
+    city = models.TextField()
+    zipCode = models.IntegerField()
+
+    mainContact = models.CharField(max_length=MAX_LENGTH)
+    mainEmail = models.EmailField(max_length=MAX_LENGTH, blank=True)
